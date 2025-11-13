450db65b08e12ad52982baedc62704afeaba43ce
asimon84 <ajsimon4@yahoo.com>
2025-11-12 18:40:35 -0800

    assets added

---
+++ public/build/assets/input-error-DDufHzbJ.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-NVwWnSCW.js";import{c as o}from"./createLucideIcon-B5MJEiDm.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
